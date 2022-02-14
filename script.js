23c4cd248373958e5d5e0464d9138b71c3ed2e9c
Siva sai Prakash <ssprayudu121@gmail.com>
2022-02-14 13:32:31 +0530

    'updated_background_bug'

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Input
 let search = document.getElementById('search-bar');
+let bod = document.querySelector('body');
 
 // Book Classes
 let books = document.querySelectorAll('.img');
@@ -19,9 +20,12 @@ function searchNow() {
     books.forEach((book) => {
         if (book.querySelector('a').textContent.trim().toLowerCase().indexOf(value) != -1) {
             book.style.display = '';
+            bod.style.background = '';
         }
         else {
             book.style.display = 'none';
+            bod.style.background = '';
         }
+        bod.style.background = 'linear-gradient(120deg,rgba(51, 88, 255, 0.952),rgb(95, 226, 95)) no-repeat center center/cover'
     })
 }
